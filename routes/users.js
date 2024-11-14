ce97df994b15c2d8051cb6c4ea71e88ac40e2ec3
yeom0920 <you920671@naver.com>
2024-11-14 10:11:50 +0900

    add

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+// 로그인 라우터
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: '로그인', pageName:'users/login.ejs' });
+});
+
+// 회원가입 라우터
+router.get('/join', function(req, res, next) {
+  res.render('index', { title: '회원가입', pageName:'users/join.ejs' });
+});
+
+// 장바구니 라우터
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: '장바구니', pageName:'users/cart.ejs' });
+});
+
+module.exports = router;
